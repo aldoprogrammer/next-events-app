9ed3d0df24da38235e6d2d96125fdb0d77c0e115
aldoprogrammer <aldobesma@gmail.com>
2023-12-10 17:54:44 +0700

    feat: update db mongo repo

--- providers/EventDBRepository.ts
+++ providers/EventDBRepository.ts
@@ -33,31 +33,4 @@ export async function allEvents({
   return response.data;
 }
 
-export function getEventById(_id: string): Event | undefined {
-  const connectDb: string = "mongo";
-  const url =
-  connectDb === "mongo"
-    ? `${API_MONGODB_URL}/events/${_id}`
-    : `${API_POSTGRESTDB_URL}/events/${_id}`;
-  return url.find((event) => event.id === _id);
-}
 
-export async function findEventById(eventId: string): Promise<Event | null> {
-  const connectDb: string = "mongo";
-
-  // Switch the backend api url from Mongo to Postgrest
-  const url =
-    connectDb === "mongo"
-      ? `${API_MONGODB_URL}/events/${eventId}`
-      : `${API_POSTGRESTDB_URL}/events/${eventId}`;
-
-  try {
-    const response = await axios.get(url);
-    console.log(JSON.stringify(response.data, null, 2));
-    return response.data;
-  } catch (error) {
-    // Handle errors, for example, log the error and return null
-    console.error("Error finding event by ID:", error);
-    return null;
-  }
-}
